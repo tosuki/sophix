37cacae143fc2d0cd81bd281b23c0eb02ca02cd9
tosuki <okaabe2006@gmail.com>
2025-02-09 20:52:38 -0300

    feat: remove useless include in sophix file

--- src/sophix.c
+++ src/sophix.c
@@ -3,14 +3,7 @@
 #include <X11/cursorfont.h>
 #include <X11/Xutil.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "./wm.h"
-#include "./lib/utils.h"
-
-#include "./cursor.h"
-#include "./keyboard.h"
 
 int main() {
     WindowManager* windowManager = createWindowManager();
